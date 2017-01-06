fix: casting error in 2stream runner causing incorrect results remove: target offset in cuave bimodal with diff runner refactor: rename names of layers in 2 stream model

runners/2stream.py
@@ -45,32 +45,6 @@ def configure_theano():
     sys.setrecursionlimit(10000)
 
 
-def evaluate_model(X_val, y_val, mask_val, X_diff_val, window_size, eval_fn):
-    """
-    Evaluate a lstm model
-    :param X_val: validation inputs
-    :param y_val: validation targets
-    :param mask_val: input masks for variable sequences
-    :param X_diff_val: validation inputs diff image
-    :param window_size: size of window for computing delta coefficients
-    :param eval_fn: evaluation function
-    :return: classification rate, confusion matrix
-    """
-    output = eval_fn(X_val, mask_val, X_diff_val, window_size)
-    no_gps = output.shape[1]
-    confusion_matrix = np.zeros((no_gps, no_gps), dtype='int')
-
-    ix = np.argmax(output, axis=1)
-    c = ix == y_val
-    classification_rate = np.sum(c == True) / float(len(c))
-
-    # construct the confusion matrix
-    for i, target in enumerate(y_val):
-        confusion_matrix[target, ix[i]] += 1
-
-    return classification_rate, confusion_matrix
-
-
 def evaluate_model2(X_val, y_val, mask_val, X_diff_val, window_size, eval_fn):
     """
     Evaluate a lstm model
@@ -107,6 +81,32 @@ def evaluate_model2(X_val, y_val, mask_val, X_diff_val, window_size, eval_fn):
     return classification_rate, confusion_matrix
 
 
+def presplit_dataprocessing(data_matrix, vidlens, config, stream_name, **kwargs):
+    reorderdata = config.getboolean(stream_name, 'reorderdata')
+    diffimage = config.getboolean(stream_name, 'diffimage')
+    meanremove = config.getboolean(stream_name, 'meanremove')
+    samplewisenormalize = config.getboolean(stream_name, 'samplewisenormalize')
+    if reorderdata:
+        imagesize = kwargs['imagesize']
+        data_matrix = reorder_data(data_matrix, imagesize)
+    if samplewisenormalize:
+        data_matrix = normalize_input(data_matrix)
+    if meanremove:
+        data_matrix = sequencewise_mean_image_subtraction(data_matrix, vidlens)
+    if diffimage:
+        data_matrix = compute_diff_images(data_matrix, vidlens)
+    return data_matrix
+
+
+def postsplit_datapreprocessing(train_X, val_X, test_X, config, stream_name):
+    featurewisenormalize = config.getboolean(stream_name, 'featurewisenormalize')
+    if featurewisenormalize:
+        train_X, mean, std = featurewise_normalize_sequence(train_X)
+        val_X = (val_X - mean) / std
+        test_X = (test_X - mean) / std
+    return train_X, val_X, test_X
+
+
 def parse_options():
     options = dict()
     options['config'] = 'config/bimodal_meanrm_raw_diff.ini'
@@ -135,38 +135,48 @@ def main():
 
     print('Reading Config File: {}...'.format(config_file))
     print(config.items('data'))
-    print(config.items('models'))
+    print(config.items('stream1'))
+    print(config.items('stream2'))
+    print(config.items('lstm_classifier'))
     print(config.items('training'))
 
     print('preprocessing dataset...')
-    data = load_mat_file(config.get('data', 'images'))
-    stream1 = config.get('models', 'stream1')
-    stream2 = config.get('models', 'stream2')
-    stream1_dim = config.getint('models', 'stream1_dim')
-    stream2_dim = config.getint('models', 'stream2_dim')
-    stream1_shapes = config.get('models', 'stream1_shapes')
-    stream2_shapes = config.get('models', 'stream2_shapes')
-    stream1_nonlinearities = config.get('models', 'stream1_nonlinearities')
-    stream2_nonlinearities = config.get('models', 'stream2_nonlinearities')
-    fusiontype = config.get('models', 'fusiontype')
-    imagesize = tuple([int(d) for d in config.get('models', 'imagesize').split(',')])
-    output_classes = config.getint('models', 'output_classes')
-    output_classnames = config.get('models', 'output_classnames').split(',')
-    lstm_size = config.getint('models', 'lstm_size')
-    matlab_target_offset = config.getboolean('models', 'matlab_target_offset')
+
+    # stream 1
+    s1_data = load_mat_file(config.get('stream1', 'data'))
+    s1_imagesize = tuple([int(d) for d in config.get('stream1', 'imagesize').split(',')])
+    s1 = config.get('stream1', 'model')
+    s1_inputdim = config.getint('stream1', 'input_dimensions')
+    s1_shape = config.get('stream1', 'shape')
+    s1_nonlinearities = config.get('stream1', 'nonlinearities')
+
+    # stream 2
+    s2_data = load_mat_file(config.get('stream2', 'data'))
+    s2_imagesize = tuple([int(d) for d in config.get('stream2', 'imagesize').split(',')])
+    s2 = config.get('stream2', 'model')
+    s2_inputdim = config.getint('stream2', 'input_dimensions')
+    s2_shape = config.get('stream2', 'shape')
+    s2_nonlinearities = config.get('stream2', 'nonlinearities')
+
+    # lstm classifier
+    fusiontype = config.get('lstm_classifier', 'fusiontype')
+    weight_init = options['weight_init'] if 'weight_init' in options else config.get('lstm_classifier', 'weight_init')
+    use_peepholes = options['use_peepholes'] if 'use_peepholes' in options else config.getboolean('lstm_classifier',
+                                                                                                  'use_peepholes')
+    windowsize = config.getint('lstm_classifier', 'windowsize')
+    output_classes = config.getint('lstm_classifier', 'output_classes')
+    output_classnames = config.get('lstm_classifier', 'output_classnames').split(',')
+    lstm_size = config.getint('lstm_classifier', 'lstm_size')
+    matlab_target_offset = config.getboolean('lstm_classifier', 'matlab_target_offset')
 
     # capture training parameters
     validation_window = int(options['validation_window']) \
         if 'validation_window' in options else config.getint('training', 'validation_window')
     num_epoch = int(options['num_epoch']) if 'num_epoch' in options else config.getint('training', 'num_epoch')
-    weight_init = options['weight_init'] if 'weight_init' in options else config.get('training', 'weight_init')
     learning_rate = options['learning_rate'] if 'learning_rate' in options \
         else config.getfloat('training', 'learning_rate')
-    use_peepholes = options['use_peepholes'] if 'use_peepholes' in options else config.getboolean('training',
-                                                                                                  'use_peepholes')
     epochsize = config.getint('training', 'epochsize')
     batchsize = config.getint('training', 'batchsize')
-    windowsize = config.getint('training', 'windowsize')
 
     weight_init_fn = las.init.GlorotUniform()
     if weight_init == 'glorot':
@@ -182,32 +192,35 @@ def main():
     val_subject_ids = read_data_split_file(config.get('training', 'val_subjects_file'))
     test_subject_ids = read_data_split_file(config.get('training', 'test_subjects_file'))
 
-    data_matrix = data['dataMatrix']
-    targets_vec = data['targetsVec'].reshape((-1,))
-    subjects_vec = data['subjectsVec'].reshape((-1,))
-    vidlen_vec = data['videoLengthVec'].reshape((-1,))
+    s1_data_matrix = s1_data['dataMatrix'].astype('float32')
+    s2_data_matrix = s2_data['dataMatrix'].astype('float32')
+    targets_vec = s1_data['targetsVec'].reshape((-1,))
+    subjects_vec = s1_data['subjectsVec'].reshape((-1,))
+    vidlen_vec = s1_data['videoLengthVec'].reshape((-1,))
 
-    data_matrix = reorder_data(data_matrix, (imagesize[0], imagesize[1]))
-
-    train_X, train_y, train_vidlens, train_subjects, \
-    val_X, val_y, val_vidlens, val_subjects, \
-    test_X, test_y, test_vidlens, test_subjects = split_seq_data(data_matrix, targets_vec, subjects_vec, vidlen_vec,
-                                                                 train_subject_ids, val_subject_ids, test_subject_ids)
     if matlab_target_offset:
-        train_y -= 1
-        val_y -= 1
-        test_y -= 1
+        targets_vec -= 1
 
-    train_X_diff = compute_diff_images(train_X, train_vidlens)
-    val_X_diff = compute_diff_images(val_X, val_vidlens)
-    test_X_diff = compute_diff_images(test_X, test_vidlens)
+    s1_data_matrix = presplit_dataprocessing(s1_data_matrix, vidlen_vec, config, 'stream1', imagesize=s1_imagesize)
+    s2_data_matrix = presplit_dataprocessing(s2_data_matrix, vidlen_vec, config, 'stream2', imagesize=s2_imagesize)
 
-    train_X = sequencewise_mean_image_subtraction(train_X, train_vidlens)
-    val_X = sequencewise_mean_image_subtraction(val_X, val_vidlens)
-    test_X = sequencewise_mean_image_subtraction(test_X, test_vidlens)
+    s1_train_X, s1_train_y, s1_train_vidlens, s1_train_subjects, \
+    s1_val_X, s1_val_y, s1_val_vidlens, s1_val_subjects, \
+    s1_test_X, s1_test_y, s1_test_vidlens, s1_test_subjects = split_seq_data(s1_data_matrix, targets_vec, subjects_vec,
+                                                                             vidlen_vec, train_subject_ids,
+                                                                             val_subject_ids, test_subject_ids)
 
-    ae1 = load_decoder(stream1, stream1_shapes, stream1_nonlinearities)
-    ae2 = load_decoder(stream2, stream2_shapes, stream2_nonlinearities)
+    s2_train_X, s2_train_y, s2_train_vidlens, s2_train_subjects, \
+    s2_val_X, s2_val_y, s2_val_vidlens, s2_val_subjects, \
+    s2_test_X, s2_test_y, s2_test_vidlens, s2_test_subjects = split_seq_data(s2_data_matrix, targets_vec, subjects_vec,
+                                                                             vidlen_vec, train_subject_ids,
+                                                                             val_subject_ids, test_subject_ids)
+
+    s1_train_X, s1_val_X, s1_test_X = postsplit_datapreprocessing(s1_train_X, s1_val_X, s1_test_X, config, 'stream1')
+    s2_train_X, s2_val_X, s2_test_X = postsplit_datapreprocessing(s2_train_X, s2_val_X, s2_test_X, config, 'stream2')
+
+    ae1 = load_decoder(s1, s1_shape, s1_nonlinearities)
+    ae2 = load_decoder(s2, s2_shape, s2_nonlinearities)
 
     # IMPT: the encoder was trained with fortan ordered images, so to visualize
     # convert all the images to C order using reshape_images_order()
@@ -223,9 +236,9 @@ def main():
     targets = T.imatrix('targets')
 
     print('constructing end to end model...')
-    network, l_fuse = adenet_v2_2.create_model(ae1, ae2, (None, None, stream1_dim), inputs1,
+    network, l_fuse = adenet_v2_2.create_model(ae1, ae2, (None, None, s1_inputdim), inputs1,
                                                (None, None), mask,
-                                               (None, None, stream2_dim), inputs2,
+                                               (None, None, s2_inputdim), inputs2,
                                                lstm_size, window, output_classes, fusiontype,
                                                w_init_fn=weight_init_fn,
                                                use_peepholes=use_peepholes)
@@ -262,21 +275,21 @@ def main():
     best_val = float('inf')
     best_cr = 0.0
 
-    datagen = gen_lstm_batch_random(train_X, train_y, train_vidlens, batchsize=batchsize)
-    integral_lens = compute_integral_len(train_vidlens)
+    datagen = gen_lstm_batch_random(s1_train_X, s1_train_y, s1_train_vidlens, batchsize=batchsize)
+    integral_lens = compute_integral_len(s1_train_vidlens)
 
-    val_datagen = gen_lstm_batch_random(val_X, val_y, val_vidlens, batchsize=len(val_vidlens))
-    test_datagen = gen_lstm_batch_random(test_X, test_y, test_vidlens, batchsize=len(test_vidlens))
+    val_datagen = gen_lstm_batch_random(s1_val_X, s1_val_y, s1_val_vidlens, batchsize=len(s1_val_vidlens))
+    test_datagen = gen_lstm_batch_random(s1_test_X, s1_test_y, s1_test_vidlens, batchsize=len(s1_test_vidlens))
 
     # We'll use this "validation set" to periodically check progress
     X_val, y_val, mask_val, idxs_val = next(val_datagen)
-    integral_lens_val = compute_integral_len(val_vidlens)
-    X_diff_val = gen_seq_batch_from_idx(val_X_diff, idxs_val, val_vidlens, integral_lens_val, np.max(val_vidlens))
+    integral_lens_val = compute_integral_len(s1_val_vidlens)
+    X_diff_val = gen_seq_batch_from_idx(s2_val_X, idxs_val, s1_val_vidlens, integral_lens_val, np.max(s1_val_vidlens))
 
     # we use the test set to check final classification rate
     X_test, y_test, mask_test, idxs_test = next(test_datagen)
-    integral_lens_test = compute_integral_len(test_vidlens)
-    X_diff_test = gen_seq_batch_from_idx(test_X_diff, idxs_test, test_vidlens, integral_lens_test, np.max(test_vidlens))
+    integral_lens_test = compute_integral_len(s1_test_vidlens)
+    X_diff_test = gen_seq_batch_from_idx(s2_test_X, idxs_test, s1_test_vidlens, integral_lens_test, np.max(s1_test_vidlens))
 
     # reshape the targets for validation
     y_val_evaluate = y_val
@@ -289,8 +302,8 @@ def main():
             # repeat targets based on max sequence len
             y = y.reshape((-1, 1))
             y = y.repeat(m.shape[-1], axis=-1)
-            X_diff = gen_seq_batch_from_idx(train_X_diff, batch_idxs,
-                                            train_vidlens, integral_lens, np.max(train_vidlens))
+            X_diff = gen_seq_batch_from_idx(s2_train_X, batch_idxs,
+                                            s1_train_vidlens, integral_lens, np.max(s1_train_vidlens))
             print_str = 'Epoch {} batch {}/{}: {} examples using adam'.format(
                 epoch + 1, i + 1, epochsize, len(X))
             print(print_str, end='')